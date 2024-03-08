1bca7843acff6873865937d9172cecae767df181
effiecancode <faitheffie25@gmail.com>
2024-03-08 03:03:37 +0300

    add class method --fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,6 @@
 import ClassRoom from './0-classroom';
-//  const ClassRoom = require("./0-classroom").default;
 
-function initializeRooms() {
-    const obj = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-    return obj;
+export default function initializeRooms() {
+  const Arr = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return Arr;
 }
-
-export default initializeRooms();
-
-// test
-// console.log(initializeRooms());
