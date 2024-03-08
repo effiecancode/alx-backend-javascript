c9dc5cdf2b2c5eaf8e2c3aad4da4576c0fa031af
effiecancode <faitheffie25@gmail.com>
2024-03-08 03:19:35 +0300

    implement static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amountclear(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
